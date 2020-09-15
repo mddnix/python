72a7583399555182866640e790ad596dd4379402
Madhusudhan D Desai <mddnix@gmail.com>
2020-09-15 13:25:33 +0530

    Inserted header

--- hvc.py
+++ hvc.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
+""" Encodes video file(s) recursively to HEVC 10Bit video. """
 
-import os, glob
+from glob import glob
+
+__author__ = "Madhusudhan D Desai"
+__copyright__ = "Copyright 2020, mddnix"
+__license__ = "GPL"
+__version__ = "1.0"
+__maintainer__ = "Madhusudhan D Desai"
+__email__ = "mddnix@gmail.com"
+__status__ = "Production"
 
 extn = ('*.mp4', '*.pdf')
 files = []
 
 for file in extn:
-    files.extend(glob.glob("**/*" + file, recursive = True))
+    files.extend(glob("**/*" + file, recursive = True))
 
 #print(files)
 for i in files:
